fcf5f526a6055a759f7fd34fb7ebc6f7d5122ec9
Kevin <knphillips01@gmail.com>
2020-11-25 17:10:39 -0600

    reversestring done

---
+++ reversestring/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  arr.join('');
+  return arr.join('')
+}
+
+module.exports = reverse;
