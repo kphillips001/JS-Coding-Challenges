4d6465e0148947414217aecb0ac38934dd0911a8
Kevin <knphillips01@gmail.com>
2020-11-28 12:41:34 -0600

    steps solved

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Pseudo 
+// From 0 to n (iterate through rows)
+  // Create an empty string, 'stair'
+  // From 0 to n (iterate through columns)
+    //IF the current column is equal to or less than the current row
+      // Add a "#" to 'stair'
+    //ELSE
+      // Add a space to 'stair
+  // Console log 'stair'
+
+// Will have rows and columns to iterate through (row), (column)
+
+function steps(n) {
+  for (let row = 0; row < n; row++){
+    let stair = '';
+
+    for (let column = 0; column < n; column++){
+      if(column <= row){
+        stair += '#'
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
